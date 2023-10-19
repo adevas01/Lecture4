5da93c7eca002a97f03c8cea1268da48f26572f8
adevas01 <adevas01@mail.bbk.ac.uk>
2023-10-19 19:21:21 +0100

    Commit

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person {
+  private String name;
+  private int age;
+
+  public Person(String name) {
+    this.name = name;
+    this.age = 0;
+  }
+
+  public String getName() {
+    return name;
+  } 
+
+  public void birthday() {
+    age++;  //????
+    System.out.println("Happy Birthday!");
+  }   
+}
