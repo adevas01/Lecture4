7cc7e4b1bafba216e203f904932af6502cd3cdc7
adevas01 <adevas01@mail.bbk.ac.uk>
2023-10-19 19:21:33 +0100

    Commit

---
+++ Tester.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Tester {
+  public static void main(String[] args) {
+
+    Rectangle rectangle = new Rectangle(2.8, 5.0);
+    double area = rectangle.area();
+    System.out.println("The area of the rectangle is: " + area);
+  }  
+}
