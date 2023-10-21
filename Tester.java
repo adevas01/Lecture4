052b932893491758df537c555096d31245415701
adevas01 <adevas01@mail.bbk.ac.uk>
2023-10-21 13:22:43 +0100

    Modified

--- Tester.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Tester {
-  public static void main(String[] args) {
-
-    Rectangle rectangle = new Rectangle(2.8, 5.0);
-    double area = rectangle.area();
-    System.out.println("The area of the rectangle is: " + area);
-  }  
-}
