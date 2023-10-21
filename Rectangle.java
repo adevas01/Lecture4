1c39cf92c0e9f80a6cc4281b24db9da5deefdb55
adevas01 <adevas01@mail.bbk.ac.uk>
2023-10-21 13:22:14 +0100

    Modified

--- Rectangle.java
+++ Rectangle.java
@@ -12,4 +12,16 @@ public class Rectangle {
   public double area() {
     return width * height; 
   }
+  public void setWidth(double width) {
+    this.width = width;
   }
+  public void setHeight(double height) {
+    this.height = height;
+  }
+  public double getWidth() {
+    return width;
+  }
+  public double getHeight() {
+    return height; 
+  }
+}
