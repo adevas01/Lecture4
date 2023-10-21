7e0327bd09f3faa85f8fd4879799a472878da01e
adevas01 <adevas01@mail.bbk.ac.uk>
2023-10-21 13:45:13 +0100

    Changed

--- PersonTester.java
+++ PersonTester.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 public class PersonTester {
   public static void main(String[] args) {
-
-    Person person = new Person("John Smith");
+    System.out.println("Please, enter your name: ");
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.next(); 
+    Person person = new Person(name);
+   
     System.out.println(person.getName() + " is having a birthday!");
     person.birthday();
   }  
